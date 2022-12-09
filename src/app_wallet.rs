6bf771c84b218f425b03a10b0c3e6a699340285f
志宇 <hello@evanlinjin.me>
2022-12-09 14:57:39 +0800

    Add `AppWallet::new`

--- src/app_wallet.rs
+++ src/app_wallet.rs
@@ -3,12 +3,26 @@ use std::{
     rc::Rc,
 };
 
-use bdk::database::MemoryDatabase;
+use bdk::{bitcoin::Network, database::MemoryDatabase, descriptor::IntoWalletDescriptor};
 
 #[derive(Clone)]
 pub struct AppWallet(Rc<RefCell<bdk::Wallet<MemoryDatabase>>>);
 
 impl AppWallet {
+    pub fn new<E: IntoWalletDescriptor>(
+        descriptor: E,
+        change_descriptor: Option<E>,
+        network: Network,
+    ) -> Result<Self, bdk::Error> {
+        let wallet = bdk::Wallet::new(
+            descriptor,
+            change_descriptor,
+            network,
+            MemoryDatabase::new(),
+        )?;
+        Ok(Self(Rc::new(RefCell::new(wallet))))
+    }
+
     pub fn borrow(&self) -> Ref<bdk::Wallet<MemoryDatabase>> {
         self.0.borrow()
     }
