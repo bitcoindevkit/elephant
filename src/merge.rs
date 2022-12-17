c6ade471c98f014158e2a85050720ab230bc4940
Daniela Brozzoni <danielabrozzoni@protonmail.com>
2022-12-17 16:11:10 +0100

    Improve the merge tab

--- src/merge.rs
+++ src/merge.rs
@@ -6,9 +6,10 @@ use web_sys::HtmlInputElement;
 use yew::prelude::*;
 
 pub enum Msg {
-    PsbtChanged(usize, InputEvent),
+    PsbtInputChanged(InputEvent),
     DeletePsbt(usize),
-    NewPsbtField,
+    //NewPsbtField,
+    AddPsbt,
     Merge,
     BroadcastTriggered,
     BroadcastFinished(Result<(), String>),
@@ -20,7 +21,9 @@ pub struct Props {
 }
 
 pub struct Merge {
-    psbts: Vec<(Option<Result<PartiallySignedTransaction, String>>, usize)>,
+    psbt_input: Option<Result<PartiallySignedTransaction, String>>,
+    psbt_input_text: String,
+    psbts: Vec<(PartiallySignedTransaction, usize)>,
     merged_psbt: Option<Result<PartiallySignedTransaction, String>>,
     wallet: AppWallet,
     is_broadcasting: bool,
@@ -35,7 +38,9 @@ impl Component for Merge {
     fn create(ctx: &Context<Self>) -> Self {
         let props = ctx.props();
         Self {
-            psbts: vec![(None, 0)],
+            psbt_input: None,
+            psbt_input_text: "".to_string(),
+            psbts: vec![],
             merged_psbt: None,
             wallet: props.wallet.clone(),
             key_n: 0,
@@ -46,10 +51,9 @@ impl Component for Merge {
 
     fn view(&self, ctx: &Context<Self>) -> Html {
         let merge = ctx.link().callback(|_| Msg::Merge);
-        let add_psbt = ctx.link().callback(|_| Msg::NewPsbtField);
+        let add_psbt = ctx.link().callback(|_| Msg::AddPsbt);
         let broadcast = ctx.link().callback(|_| Msg::BroadcastTriggered);
-        let mut add_psbt_disabled = false;
-        let mut merge_disabled = false;
+        let merge_disabled = self.psbts.is_empty();
         let (merged_psbt, merge_error) = match &self.merged_psbt {
             Some(Ok(p)) => (p.clone().to_string(), "".to_string()),
             Some(Err(e)) => ("".to_string(), e.to_string()),
@@ -58,67 +62,52 @@ impl Component for Merge {
         let broadcast_disabled =
             self.merged_psbt.is_none() || merge_error != "" || self.is_broadcasting;
         let is_invalid = if merge_error != "" { "is-invalid" } else { "" };
+        let (add_psbt_disabled, psbt_input_is_invalid) = match self.psbt_input {
+            Some(Err(_)) => (true, "is-invalid"),
+            Some(Ok(_)) => (false, ""),
+            None => (true, ""),
+        };
 
         let broadcast_result_msg = match &self.broadcast_result {
-            Some(Ok(())) => format!("Broadcast is successful! Yayyyy!"),
-            Some(Err(err)) => format!("You failed! Error: {}", err.to_string()),
+            Some(Ok(())) => format!(
+                "Successfully broadcasted transaction with txid: {:?}",
+                self.merged_psbt
+                    .as_ref()
+                    .unwrap()
+                    .as_ref()
+                    .unwrap()
+                    .unsigned_tx
+                    .txid()
+            ),
+            Some(Err(err)) => format!("Broadcast failed with error: {}", err.to_string()),
             None => format!(""),
         };
+        let oninput = ctx
+            .link()
+            .callback(move |e: InputEvent| Msg::PsbtInputChanged(e));
 
         html! {
             <div class="daniela">
-                <label for="accordionExample" class="form-label">{"Paste here the PSBTs to merge:"}</label>
-                <div class="accordion" id="accordionExample">
-                    {
-                        for self.psbts.iter().enumerate().map(|(i, (psbt, key))| {
-                            let oninput = ctx.link().callback(move |e: InputEvent| Msg::PsbtChanged(i, e));
-                            let delete_psbt = ctx.link().callback(move |_| Msg::DeletePsbt(i));
-                            let (is_invalid, button_disabled, annotation_title) = match psbt {
-                                Some(Ok(_)) => ("", false, "Transaction"),
-                                Some(Err(_)) => ("is-invalid", true, "Invalid PSBT!"),
-                                None => ("", true, "(Empty)"),
-                            };
-                            add_psbt_disabled |= button_disabled;
-                            merge_disabled |= button_disabled;
-                            let heading_name = format!("heading{}", i);
-                            let collapse_name = format!("collapse{}", i);
-                            let collapse_name_id = format!("#collapse{}", i);
-                            let psbt_error = match psbt {
-                                Some(Err(e)) => e.to_string(),
-                                _ => "".to_string(),
-                            };
-
-                            html! {
-                                <div class="accordion-item" key={*key}>
-                                <h2 class="accordion-header" id={heading_name.clone()}>
-                                  <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={collapse_name_id.clone()} aria-expanded="true" aria-controls={collapse_name.clone()}>
-                                  { annotation_title }
-                                  </button>
-                                </h2>
-                                <div id={collapse_name.clone()} class="accordion-collapse collapse show" aria-labelledby={heading_name.clone()} data-bs-parent="#accordionExample">
-                                  <div class="accordion-body">
-                                    <div>
-                                        <textarea class={classes!("form-control", "daniela-textarea", is_invalid).to_string()} rows="5" {oninput}></textarea>
-                                        <div class="invalid-feedback">
-                                        { format!("Please enter a valid PSBT: {}", psbt_error) }
-                                        </div>
-                                    </div>
-                                    <button class="btn btn-danger" onclick={delete_psbt} disabled={ self.psbts.len() == 1 }>{ "Delete" }</button>
-                                  </div>
-                                </div>
-                                </div>
-                            }
-                        })
-                    }
+                <div class="same-line">
+                    <input type="text" style="margin-right: 5px" class={classes!("form-control", psbt_input_is_invalid)} id="addPsbt" {oninput} value={self.psbt_input_text.clone()} placeholder="Paste your PSBT here..."/>
+                    <button class="btn btn-primary" onclick={add_psbt} disabled={ add_psbt_disabled }> <i class="bi bi-plus-lg"></i> </button>
                 </div>
-                <div>
-                    <button class="btn btn-primary daniela-button" onclick={add_psbt} disabled={ add_psbt_disabled }>{ "Add PSBT" }</button>
-                </div>
+                {
+                    for self.psbts.iter().enumerate().map(|(i, (psbt, key))| {
+                        let delete_psbt = ctx.link().callback(move |_| Msg::DeletePsbt(i));
+                        html! {
+                            <div class="same-line">
+                                <input type="text" style="margin-right: 5px" class="form-control" disabled=true key={*key} value={psbt.to_string()}/>
+                                <button class="btn btn-danger" onclick={delete_psbt} ><i class="bi bi-trash"></i></button>
+                            </div>
+                        }
+                    })
+                }
+                <button class="btn btn-primary daniela-button" onclick={merge} disabled={ merge_disabled }>{ "Merge" }</button>
                 <textarea class={classes!("form-control","daniela-textarea",is_invalid)} id="merged-psbt-textarea" rows="10" readonly=true value={merged_psbt}></textarea>
                 <div class="invalid-feedback">
                 { format!("Error merging PSBTs: {}", merge_error) }
                 </div>
-                <button class="btn btn-primary daniela-button" onclick={merge} disabled={ merge_disabled }>{ "Finish" }</button>
                 <button class="btn btn-primary daniela-button" onclick={broadcast} disabled={ broadcast_disabled }>{ if self.is_broadcasting { "Broadcasting..." } else { "Broadcast" } }</button>
                 <div>
                     <label> { broadcast_result_msg } </label>
@@ -129,26 +118,6 @@ impl Component for Merge {
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::PsbtChanged(i, e) => {
-                let psbt = e.target_unchecked_into::<HtmlInputElement>().value();
-                log::info!("Psbt changed! {:?}", psbt);
-                self.merged_psbt = None;
-                if psbt == "" {
-                    self.psbts[i].0 = None;
-                } else {
-                    self.psbts[i].0 = Some(
-                        PartiallySignedTransaction::from_str(&psbt).map_err(|e| e.to_string()),
-                    );
-                }
-                log::info!("Psbt parsing: {:?}", self.psbts);
-                true
-            }
-            Msg::NewPsbtField => {
-                log::info!("Add new PSBT field");
-                self.key_n += 1;
-                self.psbts.push((None, self.key_n));
-                true
-            }
             Msg::DeletePsbt(i) => {
                 log::info!("Delete");
                 self.psbts.remove(i);
@@ -156,9 +125,9 @@ impl Component for Merge {
             }
             Msg::Merge => {
                 log::info!("Merge");
-                let mut temp = self.psbts[0].clone().0.unwrap().unwrap();
+                let mut temp = self.psbts[0].clone().0;
                 for psbt in &self.psbts[1..] {
-                    if let Err(e) = temp.combine(psbt.clone().0.unwrap().unwrap()) {
+                    if let Err(e) = temp.combine(psbt.clone().0) {
                         self.merged_psbt = Some(Err(e.to_string()));
                         return true;
                     }
@@ -170,22 +139,54 @@ impl Component for Merge {
                 log::info!("Broadcast");
                 self.is_broadcasting = true;
                 let mut merged_psbt = self.merged_psbt.clone().unwrap().unwrap();
-                let tx = self
+                let finalized = self
                     .wallet
                     .borrow()
                     .0
                     .finalize_psbt(&mut merged_psbt, SignOptions::default());
-                let tx = merged_psbt.extract_tx();
-                let wallet_cloned = self.wallet.0.clone();
-                ctx.link().send_future(async move {
-                    let res = wallet_cloned.borrow().1.broadcast(&tx).await;
-                    Msg::BroadcastFinished(res.map_err(|e| e.to_string()))
-                });
+                match finalized {
+                    Ok(true) => {
+                        let tx = merged_psbt.extract_tx();
+                        let wallet_cloned = self.wallet.0.clone();
+                        ctx.link().send_future(async move {
+                            let res = wallet_cloned.borrow().1.broadcast(&tx).await;
+                            Msg::BroadcastFinished(res.map_err(|e| e.to_string()))
+                        });
+                    }
+                    Ok(false) => {
+                        self.broadcast_result = Some(Err("Can't finalize PSBT".to_string()));
+                    }
+                    Err(e) => {
+                        self.broadcast_result =
+                            Some(Err(format!("Error when finalizing PSBT: {}", e)));
+                    }
+                }
                 true
             }
             Msg::BroadcastFinished(res) => {
                 self.is_broadcasting = false;
                 self.broadcast_result = Some(res);
+                true
+            }
+            Msg::AddPsbt => {
+                let psbt = self.psbt_input.clone().unwrap().unwrap();
+                self.key_n += 1;
+                self.psbts.push((psbt, self.key_n));
+                self.psbt_input_text = "".to_string();
+                true
+            }
+            Msg::PsbtInputChanged(e) => {
+                let psbt = e.target_unchecked_into::<HtmlInputElement>().value();
+                self.psbt_input_text = psbt.clone();
+                log::info!("Psbt changed! {:?}", &psbt);
+                if &psbt == "" {
+                    self.psbt_input = None;
+                } else {
+                    self.psbt_input = Some(
+                        PartiallySignedTransaction::from_str(&psbt).map_err(|e| e.to_string()),
+                    );
+                }
+                log::info!("Psbt parsing: {:?}", self.psbts);
                 true
             }
         }
